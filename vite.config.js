618b332e04f32cffd4d2ed8e16a92393177afb62
kryptocodes <srivatsantb@gmail.com>
2025-10-11 16:22:07 +0530

    chore: update dir
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
